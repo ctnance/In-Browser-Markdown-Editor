2b16e52d8b8a97c38c6cce38477698b823a257d1
Christopher Nance <ctnance0@gmail.com>
2022-05-07 18:14:35 -0400

    markdown context add in delete note modal

--- src/components/DeleteNoteModal.tsx
+++ src/components/DeleteNoteModal.tsx
@@ -1,5 +1,6 @@
 import { FC, useContext } from "react";
 import { ThemeContext } from "../ThemeContext";
+import { MarkdownContext } from "../MarkdownContext";
 
 interface Props {
   isActive: boolean,
@@ -8,16 +9,19 @@ interface Props {
 
 const DeleteNoteModal: FC<Props> = ({ isActive, toggleSelf }) => {
   const { isDarkTheme } = useContext(ThemeContext);
+  const { markdownNotes, activeNoteId } = useContext(MarkdownContext);
+
+  const activeNote = markdownNotes.find(note => note.id === activeNoteId)
 
   function exitModal(e: React.MouseEvent<HTMLDivElement>) {
     e.currentTarget === e.target && toggleSelf();
   }
 
   return (
-    <div className="delete-note-modal--container" onClick={exitModal}>
-      <div className={`delete-note-modal ${isDarkTheme ? "dark" : "light"}`}>
+    <div className={`delete-note-modal--container ${isDarkTheme ? "dark" : "light"}`} onClick={exitModal}>
+      <div className="delete-note-modal">
         <h4 className="delete-note-modal--title">Delete this document?</h4>
-        <p className="delete-note-modal--content">Are you sure you want to delete the `welcome.md` document and its contents? This action cannot be reversed.</p>
+        <p className="delete-note-modal--content">Are you sure you want to delete the `{activeNote?.name}` document and its contents? This action cannot be reversed.</p>
         <button className="btn delete-note-modal--btn" onClick={() => {/* TODO: Delete Note Here */ }}>Confirm & Delete</button>
       </div>
     </div>
