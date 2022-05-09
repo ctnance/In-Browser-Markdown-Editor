d4dd6986ea9a805a89f62f33fe880866c1649b06
Christopher Nance <ctnance0@gmail.com>
2022-05-09 18:52:34 -0400

    markdown preview functionality added

--- src/components/PreviewDisplay.tsx
+++ src/components/PreviewDisplay.tsx
@@ -1,13 +1,27 @@
-import { useContext } from "react";
-import { FC } from "react";
+import { FC, useState, useEffect, useContext } from "react";
 import { ThemeContext } from "../ThemeContext";
+import { MarkdownContext } from "../MarkdownContext";
+
+import Markdown from "markdown-to-jsx";
 
 const PreviewDisplay: FC = () => {
+  const [text, setText] = useState("");
   const { isDarkTheme } = useContext(ThemeContext);
+  const { markdownNotes, activeNoteId } = useContext(MarkdownContext);
+
+  useEffect(() => {
+    markdownNotes.forEach(note => {
+      if (note.id === activeNoteId) {
+        setText(note.content);
+      }
+    })
+  }, [activeNoteId])
 
   return (
     <div className={`preview-container ${isDarkTheme ? "dark" : "light"}`}>
-      <h1>PREVIEW IN PROGRESS</h1>
+      <Markdown>
+        {text}
+      </Markdown>
     </div>
   )
 }
